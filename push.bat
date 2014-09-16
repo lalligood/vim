d92378d5b0b3bc846a10ad0c37763f3fe71b75a2
lalligood <lalligood@rx30.com>
2014-09-16 10:46:15 -0400

    Better SQL syntax highlighting support with sql.vim. Improved push.bat
    Someone on reddit posted about wanting better SQL syntax highlighting
    support in vim. Found sql.vim. Made changes to _gvimrc, .gvimrc, & .vimrc
    to support this file.
    
    Discovered that syntax files & colors schemes were not being copied
    properly in push.bat. No pushes files to correct locations.

--- push.bat
+++ push.bat
@@ -1,3 +1,4 @@
 copy _* "C:\Program Files (x86)\Vim\"
-copy *.vim "C:\Program Files (x86)\Vim\vimfiles\colors"
+copy vim\colors\* "C:\Program Files (x86)\Vim\vimfiles\colors"
+copy vim\syntax\* "C:\Program Files (x86)\Vim\vimfiles\syntax"
 pause

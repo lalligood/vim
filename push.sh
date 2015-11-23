2a31cb96eaae31b126c5a4971cfd3cbebbe25c86
Lance Alligood <lalligood@gmail.com>
2015-11-23 09:57:16 -0500

    push.sh: Boiled checking/creating symlinks to a single function

--- push.sh
+++ push.sh
@@ -3,27 +3,30 @@
 FONTDIR=$HOME/.fonts
 currdir=`pwd`
 
-function makedirsymlink {
-if [ ! -d $1 ]; then
-    echo $1 directory does not exist. Creating directory...
-    mkdir $1
-    echo $1 directory created successfully
-else
-    echo $1 already exists. Skipping...
-fi
-}
 
 function makesymlink {
-if  [ ! -h $HOME/$1 ]; then
-    echo Symbolic link for $1 does not exist. Creating symlink...
-    ln -s $currdir/$2 $HOME/$1
-    echo Symbolic link for $1 created successfully.
+if [ $2 != "" ]; then
+    # Create symlink for a directory
+    if [ ! -d $1 ]; then
+        echo $1 directory does not exist. Creating directory...
+        mkdir $1
+        echo $1 directory created successfully
+    else
+        echo $1 already exists. Skipping...
+    fi
 else
-    echo Symbolic link for $1 already exists. Skipping...
+    # Create symlink for a file
+    if  [ ! -h $HOME/$1 ]; then
+        echo Symbolic link for $1 does not exist. Creating symlink...
+        ln -s $currdir/$2 $HOME/$1
+        echo Symbolic link for $1 created successfully.
+    else
+        echo Symbolic link for $1 already exists. Skipping...
+    fi
 fi
 }
 
-makedirsymlink $FONTDIR
+makesymlink $FONTDIR
 cp -fv $currdir/font/* $FONTDIR/
 
 makesymlink .vimrc .vimrc
